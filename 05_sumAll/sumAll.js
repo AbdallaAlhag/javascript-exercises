e4feb8108b7fde468af311bb12467a8320af2d58
Abdalla Alhag <alhagabdalla@gmail.com>
2023-04-06 13:49:38 -0700

    05 finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let sum = 0;
+    if(first > last){
+        let temp = last;
+        last = first;
+        first = temp;
+    }
+    // isNaN function didn't work with "90" had to switch to !Number.isInteger()
+    else if(!Number.isInteger(first) || !Number.isInteger(last) || (first < 0 || last < 0) )
+        return "ERROR";
+    
+    for(let i = first; i <= last; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
