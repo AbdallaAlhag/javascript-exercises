44e764500a306dd63ccef930b72d7adb930ccc41
Abdalla Alhag <alhagabdalla@gmail.com>
2023-04-06 08:33:15 -0700

    Finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...manyMoreArgs) {
+    for (let j = 0; j < manyMoreArgs.length; j++) {
+        for(let i = 0; i < arr.length; i++)
+        {
+            if (arr[i] === manyMoreArgs[j])
+                arr.splice(i,1);
 
+        }
+    }
+    return arr;    
 };
 
 // Do not edit below this line
