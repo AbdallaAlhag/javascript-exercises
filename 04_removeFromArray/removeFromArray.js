0b4b53e6887e95a10b45041b4d5651df87e671ac
Abdalla Alhag <alhagabdalla@gmail.com>
2023-04-06 11:06:58 -0700

    solution experiment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,35 @@
-const removeFromArray = function(arr, ...manyMoreArgs) {
-    for (let j = 0; j < manyMoreArgs.length; j++) {
-        for(let i = 0; i < arr.length; i++)
-        {
-            if (arr[i] === manyMoreArgs[j])
-                arr.splice(i,1);
+// const removeFromArray = function(arr, ...manyMoreArgs) {
+//     for (let j = 0; j < manyMoreArgs.length; j++) {
+//         for(let i = 0; i < arr.length; i++)
+//         {
+//             if (arr[i] === manyMoreArgs[j])
+//                 arr.splice(i,1);
 
-        }
-    }
-    return arr;    
-};
+//         }
+//     }
+//     return arr;    
+// };
+
+// 2nd method which uses the for each function that runs through each item and checks if our 
+// spread syntax, ...args, is in the old array and if it isn't, then we push the element in our new
+// array to get rid of any old elements.
+// const removeFromArray = function(arr, ...args)
+// {
+//     const newArray = [];
+
+//     arr.forEach((item) => {
+//        if(!args.includes(item))
+//         newArray.push(item); 
+    
+// });
+
+// return newArray;
+// };
+
+// 3rd method allows to use the filter function and include to 
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(value => !args.includes(value));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
