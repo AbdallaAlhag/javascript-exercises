eeeac771ce07f3773cc5b9da069678052d2ba231
Abdalla Alhag <alhagabdalla@gmail.com>
2023-04-06 17:47:58 -0700

    007 Complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(foo) {
+  return foo = Math.round( ((foo - 32) * (5/9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(bar) {
+  return bar = Math.round( (bar * (9/5) + 32) * 10 ) / 10;
 };
 
 // Do not edit below this line
