acbb13d9ed7bd117359b02a31f87555b87cf686e
Abdalla Alhag <alhagabdalla@gmail.com>
2023-04-08 09:31:38 -0700

    08 Complete/Verified

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,55 @@
-const add = function() {
-	
+const add = function(foo, bar) {
+	return foo + bar;
 };
 
-const subtract = function() {
-	
+const subtract = function(foo, bar) {
+	return foo - bar;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  // let total = 0;
+	// arr.forEach(element => {
+  //   total += element;
+  // });
+  // return total;
+
+  return arr.reduce((total, current) => total + current, 0);
+
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  // let total = 1;
+	// arr.forEach(element => {
+  //   total *= element;
+  // });
+  // return total;
+
+  return array.length
+    ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+    : 0;
 
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
-};
+// const factorial = function(num) {
+//   if(num === 0)
+//     return 1;
+//   let sum = num;
+// 	for(let i = num; i > 1; i--) {
+//     sum *= (i-1);
+//   }
+//   return sum;
+// };
+
+// Using recursion
+const factorial = function(num) {
+  if(num === 0)
+    return 1;
+  return num * factorial(num - 1);
+}
 
 // Do not edit below this line
 module.exports = {
