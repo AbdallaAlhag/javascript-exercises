71bd68df786735d686ea865cdac5ffc9faae5168
Abdalla Alhag <alhagabdalla@gmail.com>
2023-04-10 10:25:46 -0700

    solution answer was challenging

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    // more complicated way 
+    // let count = 0;
+    // const punctuationRegex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    // for (let i = 0; i < string.length; i++) {
+    //     if(string[string.length - 1] === punctuationRegex)
+    //         string = string.slice(0,-1);
+    //     if(string[i] === string[string.length - 1 - i]){
+    //         count++;
+    //         if(count === string.length -1)
+    //             return true;
+    //     }
+    //     else {
+    //         return false;
+    //     }
+    // }
+
+    const newString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return newString.split('').reverse().join('') === newString;
 
 };
 
