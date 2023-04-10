fb1d9ba1080964f1e062739e0f25d76c05c24afb
Abdalla Alhag <alhagabdalla@gmail.com>
2023-04-10 11:07:40 -0700

    Non-recursive solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0)
+        return "OOPS";
 
+    //return fib(num) + fib(num - 1);
+
+    let fib = [0,1];
+    for(let i = 0; i <= num - 2; i++){
+        fib.push(fib[i] + fib[i+1]);
+    }
+    return fib[num];
 };
 
+// const recursiveFactorial = function (n) {
+//     if (n === 0) {
+//       return 1;
+//     }
+//     return n * recursiveFactorial(n - 1);
+//   };
 // Do not edit below this line
 module.exports = fibonacci;
