4af4bfdea89bccf6cc1c720ab76ebb3581dbd775
Abdalla Alhag <alhagabdalla@gmail.com>
2023-04-11 13:14:37 -0700

    javascript excsercise complete!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = 0;
+    let index = 0;
+    for (let i = 0; i < people.length; i++) {
+        if(!people[i].hasOwnProperty('yearOfDeath') ){
+            let age = findAge(people[i].yearOfBirth);
+            if(oldest <  age)
+                index = i;
+                oldest = age;
+                //return people[index];
+        }
+
+        else if(people[i].yearOfDeath - people[i].yearOfBirth > oldest){
+            oldest = people[i].yearOfDeath - people[i].yearOfBirth;
+            index = i;
+        }
+    }
+    return people[index];
 
 };
+
+const findAge = function(birth, death)
+{
+    if(!death)
+        death = new Date().getFullYear();
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
